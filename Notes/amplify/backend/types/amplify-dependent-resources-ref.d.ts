78e2f41a0cb3b3a67e655238b5f2dfe784cd0700
Harshal Yanpallewar <yanpallewarharshal@gmail.com>
2021-12-12 23:16:57 +0530

    Added AWS Cognito for auth implementation

---
+++ Notes/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "notes92b9ab9a": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
