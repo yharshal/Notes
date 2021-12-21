0b7e1dbe12f631c32bd98314a0288e6414860341
Harshal Yanpallewar <yanpallewarharshal@gmail.com>
2021-12-21 13:11:49 +0530

    Added auth plugin while configuring amplify plus fetching session.

--- Notes/Notes/Backend.swift
+++ Notes/Notes/Backend.swift
@@ -8,19 +8,36 @@
 import Foundation
 import UIKit
 import Amplify
+import AmplifyPlugins
 
 class Backend {
+    
     static let shared = Backend()
+    
     static func initialize() -> Backend {
         return .shared
     }
+    
     private init() {
-      // initialize amplify
-      do {
-        try Amplify.configure()
-        print("Initialized Amplify");
-      } catch {
-        print("Could not initialize Amplify: \(error)")
-      }
+        // initialize amplify
+        do {
+            try Amplify.add(plugin: AWSCognitoAuthPlugin())
+            try Amplify.configure()
+            print("Amplify configured with auth plugin")
+        } catch {
+            print("Could not initialize Amplify: \(error)")
+        }
     }
+    
+    func fetchCurrentAuthSession() {
+        _ = Amplify.Auth.fetchAuthSession { result in
+            switch result {
+                case .success(let session):
+                    print("Is user signed in \(session.isSignedIn)")
+                case .failure(causedBy: let error):
+                    print("fetch session failed with error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
 }
